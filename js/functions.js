b5ac76a2a1b91b63f58dd12f1cf8a4dc732da135
MariusSLarsen <MariusSLarsen@users.noreply.github.com>
2020-08-03 15:54:25 +0200

    sticky nav acting weird..needs fix

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size and remove logo
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("logoo").style.display ="none";
+    document.getElementById("nav-elements").style.fontSize = "10px";
+  } else {
+      document.getElementById("logoo").style.display ="initial";
+    document.getElementById("nav-elements").style.fontSize = "12.8px";
+  }
+}
